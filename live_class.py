bc1dac954852433c792b441f05768ddc7efa61da
rahil_wisdom <mister.r432000@gmail.com>
2023-05-11 13:29:39 +0800

    test

---
+++ live_class.py
@@ -0,0 +1,99 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Masukkan bilangan bulat positif"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+for i in range(1000):
+    if i == 999:
+        print("Process 1 Finished")
+        
+for i in range(10000):
+    if i == 9999:
+        print("Process 2 Finished")
+        
+for i in range(100000):
+    if i == 99999:
+        print("Process 3 Finished")
+        
+for i in range(1000000):
+    if i == 999999:
+        print("Process 4 Finished")
+        
+for i in range(10000000):
+    if i == 9999999:
+        print("Process 5 Finished")
+        
+for i in range(100000000):
+    if i == 99999999:
+        print("Process 6 Finished")
+
+for i in range(1, 20):
+    fib = fibonacci(i)
+    if i == 19:
+        print("Process 7 Finished")
+        
+for i in range(1, 15):
+    fib = fibonacci(i)
+    if i == 14:
+        print("Process 8 Finished")
+        
+for i in range(1, 10):
+    fib = fibonacci(i)
+    if i == 9:
+        print("Process 9 Finished")
+        
+for i in range(1000000000):
+    if i == 99999999:
+        print("Process 10 Finished")
+        
+for i in range(10000000000):
+    if i == 999999999:
+        print("Process 11 Finished")
+        
+for i in range(1, 20):
+    fib = fibonacci(i)
+    if i == 19:
+        print("Process 12 Finished")
+        
+for i in range(1, 15):
+    fib = fibonacci(i)
+    if i == 14:
+        print("Process 13 Finished")
+        
+for i in range(1, 10):
+    fib = fibonacci(i)
+    if i == 9:
+        print("Process 14 Finished")
+        
+for i in range(1, 20):
+    fib = fibonacci(i)
+    if i == 19:
+        print("Process 15 Finished")
+        
+for i in range(1, 15):
+    fib = fibonacci(i)
+    if i == 14:
+        print("Process 16 Finished")
+        
+for i in range(1, 10):
+    fib = fibonacci(i)
+    if i == 9:
+        print("Process 17 Finished")
+        
+for i in range(100000000000):
+    if i == 9999999999:
+        print("Process 18 Finished")
+        
+for i in range(100000000000):
+    if i == 99999999999:
+        print("Process 19 Finished")
+        
+for i in range(1, 10):
+    fib = fibonacci(i)
+    if i == 9:
+        print("Process 20 Finished")
